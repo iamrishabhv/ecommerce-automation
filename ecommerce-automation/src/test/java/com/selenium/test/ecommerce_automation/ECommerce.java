92bcdd26d09b9bfa45d02e49e426fc82600d6b1c
Rishabh Verma <rishabhverma41@gmail.com>
2025-05-20 21:36:57 +0530

    Fixed Test Case 3 & Added Test Case 4

--- ecommerce-automation/src/test/java/com/selenium/test/ecommerce_automation/ECommerce.java
+++ ecommerce-automation/src/test/java/com/selenium/test/ecommerce_automation/ECommerce.java
@@ -75,13 +75,50 @@ public class ECommerce {
             }
         }
 		
-		WebElement addToCartBtn = wait.until(ExpectedConditions.elementToBeClickable(By.id("add-to-cart-button")));
-		addToCartBtn.click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Added to Cart')]")));
-		driver.findElement(By.xpath("//*[contains(text(),'Added to cart')]"));
+		WebElement addToCartBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div[5]/div[1]/div[4]/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div/form/div/div/div[38]/div[1]/span/span/span/input")));
+		addToCartBtn.click();			//Worked with Full XPath only
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/h1")));
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/h1"));	//Worked with Full XPath
 		
 		System.out.println("Product Successfully Added to Cart.");
 		
+		driver.quit();
+	}
+	
+	@Test	//Proceed to Checkout (up to login)
+	public void checkoutPage() throws InterruptedException {
+		WebDriverManager.chromiumdriver().setup();
+		WebDriver driver = new ChromeDriver();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.get("chrome://settings/clearBrowserData");
+		driver.findElement(By.xpath("//settings-ui")).sendKeys(Keys.ENTER);
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("Laptop");
+		driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[contains(text(),'Lenovo IdeaPad Slim 3, Intel Core i5-12450H')]")).click();
+		Thread.sleep(5000);
+		String originalWindow = driver.getWindowHandle();
+		for(String windowHandle : driver.getWindowHandles()) {
+			if (!windowHandle.equals(originalWindow)) {
+				driver.switchTo().window(windowHandle);
+				break;
+			}
+		}
+		
+		WebElement addToCrtBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div/div[5]/div[1]/div[4]/div/div[1]/div/div[1]/div/div/div[2]/div/div[2]/div/form/div/div/div[38]/div[1]/span/span/span/input")));
+		addToCrtBtn.click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/h1")));
+		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div[1]/div[2]/div[1]/div/div/div/h1"));
+		System.out.println("Product Successfully Added to Cart.");
+		WebElement prcdToChkOut = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span[1]/span/span/input")));
+		prcdToChkOut.click();
+		
+		String actualURL = driver.getCurrentUrl();
+		String expectedURL = "https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fcheckout%2Fentry%2Fcart%3FisEligibilityLogicDisabled%3D1%26referrer%3Dcart%26ref_%3Dox_sw_proceed%26proceedToCheckout%3D1%26tangoWeblabStatus%3Dtango_enable_unrec_customer%26oldCustomerId%3D0%26pipelineType%3DChewbacca%26isUnrec%3D1&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=amazon_checkout_in&openid.mode=checkid_setup&language=en_IN&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0";
+		assertEquals(actualURL, expectedURL, "Page is not correct");
 		driver.quit();
 	}
 
