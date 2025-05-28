7ae9bfd9f57fde26f61d66ba25db4dba527db1e5
iamrishabhv <rishabhverma41@gmail.com>
2025-05-28 20:09:57 +0530

    Added Tests for Sign In Page

--- ecommerce-automation/src/main/java/SoftwareTestingBoardSite/SignInPage.java
+++ ecommerce-automation/src/main/java/SoftwareTestingBoardSite/SignInPage.java
@@ -14,23 +14,38 @@ public class SignInPage {
 		this.driver = driver;
 	}
 	
-	public void SignInPage(String email, String password, String fullName) throws InterruptedException {
+	public void signIn(String email, String password, String fullName) throws InterruptedException {
 		driver.get(Constants.SignInPage);
-		driver.findElement(By.cssSelector("#email")).sendKeys(email);
-		driver.findElement(By.cssSelector("#pass")).sendKeys(password);
-		driver.findElement(By.cssSelector("#send2 > span")).click();
-		Thread.sleep(2000);
-		WebElement accName = driver.findElement(By.cssSelector("body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.greet.welcome > span"));
-		String acAccName = accName.getText();
-		System.out.println(acAccName);
-		String exAccName = "Welcome, " + fullName;
 		try {
-			acAccName.equalsIgnoreCase(exAccName);
-			System.out.println("Sign In Successful!");
-		} catch(Exception e) {
-			System.out.println("Sign In Unsuccessful!");
-			e.getStackTrace();
+			driver.findElement(By.cssSelector("#email")).sendKeys(email);
+			driver.findElement(By.cssSelector("#pass")).sendKeys(password);
+			driver.findElement(By.cssSelector("#send2 > span")).click();
+			Thread.sleep(2000);
+			if(email == null || email.isEmpty() || !email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                    "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")) {
+				System.out.println("Please Enter a Valid Email Address!");
+				return;
+			}
+			if(password == null || password.isEmpty() || !password.matches("^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$")){
+				System.out.println("Please Enter a Valid Password!");
+				return;
+			}
+			if(email.isEmpty() && password.isEmpty()) {
+				System.out.println("Please Enter the Email & Password!");
+			}
+			
+			WebElement accName = driver.findElement(By.cssSelector("body > div.page-wrapper > header > div.panel.wrapper > div > ul > li.greet.welcome > span"));
+			String acAccName = accName.getText();
+			String exAccName = "Welcome, " + fullName;
+			if (acAccName.equalsIgnoreCase(exAccName)) {
+				System.out.println("Sign In Successful!");
+	            } else {
+	                System.out.println("Sign In Unsuccessful!");
+	            }
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
-		
 	}
 }
+
+//Please enter a valid email address (Ex: johndoe@domain.com).
--- ecommerce-automation/src/test/java/software/Testing/Board/Site/SignInPageTest.java
+++ ecommerce-automation/src/test/java/software/Testing/Board/Site/SignInPageTest.java
@@ -7,9 +7,27 @@ import SoftwareTestingBoardSite.SignInPage;
 
 public class SignInPageTest extends BaseTest{
 	
-	@Test
-	public void signInTest() throws InterruptedException {
+	@Test	//TC01 - Valid Sign In
+	public void signInTest_TC1() throws InterruptedException {
 		SignInPage sip = new SignInPage(driver);
-		sip.SignInPage("John.Sina@dummy.com", "Ert@#$123", "John Sina");		
+		sip.signIn("John.Sina@dummy.com", "Ert@#$123", "John Sina");		
+	}
+	
+	@Test	//TC02 - Email & Password are NULL
+	public void signInTest_TC2() throws InterruptedException {
+		SignInPage sip = new SignInPage(driver);
+		sip.signIn(null,null,null);
+	}
+	
+	@Test	//TC03 - Check for Valid Password
+	public void signInTest_TC03() throws InterruptedException {
+		SignInPage sip = new SignInPage(driver);
+		sip.signIn("Rishabh@gmail.com", "Abc123", "Rishabh Verma");
+	}
+	
+	@Test	//TC04 - Check for Valid Email Address
+	public void singInTest_TC04() throws InterruptedException {
+		SignInPage sip = new SignInPage(driver);
+		sip.signIn("Rishabh", "Ert@#$123", "Rishabh Verma");
 	}
 }
