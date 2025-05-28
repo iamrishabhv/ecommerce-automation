12970bb16b143b0f053beccf03ab148cf8f284e9
iamrishabhv <rishabhverma41@gmail.com>
2025-05-28 20:42:06 +0530

    Added Tests & Code for CreateAccountPage

--- ecommerce-automation/src/main/java/SoftwareTestingBoardSite/CreateAccountPage.java
+++ ecommerce-automation/src/main/java/SoftwareTestingBoardSite/CreateAccountPage.java
@@ -15,25 +15,37 @@ public class CreateAccountPage {
 		this.driver = driver;
 	}
 	
-	public void CreateAccPage(String firstName, String lastName, String emailAddress, String password) {
+	public void CreateAccPage(String firstName, String lastName, String email, String password) {
 		driver.get(Constants.CreateAcc);
 		driver.findElement(By.cssSelector("#firstname")).sendKeys(firstName);
 		driver.findElement(By.cssSelector("#lastname")).sendKeys(lastName);
-		driver.findElement(By.cssSelector("#email_address")).sendKeys(emailAddress);
+		driver.findElement(By.cssSelector("#email_address")).sendKeys(email);
 		driver.findElement(By.cssSelector("#password")).sendKeys(password);
 		driver.findElement(By.cssSelector("#password-confirmation")).sendKeys(password);
 		driver.findElement(By.cssSelector("#form-validate > div > div.primary > button > span")).click();
-		WebElement RegTxt = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div"));
-		String accRegTxt = RegTxt.getText();
-		String expRegTxt = "Thank you for registering with Main Website Store.";
 		try {
-			accRegTxt.equalsIgnoreCase(expRegTxt);
-			System.out.println("Registration Successful!");
+			if(firstName == null || firstName.isEmpty() || !firstName.matches("^[A-Z][a-zA-Z]{1,29}$")) {
+				System.out.println("Please Enter a Valid First Name");
+			}
+			if(lastName == null || lastName.isEmpty() || !lastName.matches("^[A-Z][a-zA-Z]{1,29}$")) {
+				System.out.println("Please Enter a Valid Last Name");
+			}
+			if(email == null || email.isEmpty() || !email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")) {
+				System.out.println("Please Enter a Valid Email Address");
+			}
+			if(password == null || password.isEmpty() || !password.matches("^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$")) 		{
+				System.out.println("Please Enter a Valid Password");
+			}		
+			String accRegTxt = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/div[2]/div/div/div")).getText();
+			String expRegTxt = "Thank you for registering with Main Website Store.";
+				if(accRegTxt.equalsIgnoreCase(expRegTxt)) {
+					System.out.println("Registration Successful!");
+				}else {
+					System.out.println("Registration Unsuccessful!");
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
 		}
-		catch(Exception e) {
-			System.out.println("Something Went Wrong!");
-			e.getStackTrace();
-		}
-	}
-
+}
 }
--- ecommerce-automation/src/test/java/software/Testing/Board/Site/CreateAccountTest.java
+++ ecommerce-automation/src/test/java/software/Testing/Board/Site/CreateAccountTest.java
@@ -8,9 +8,33 @@ import SoftwareTestingBoardSite.CreateAccountPage;
 public class CreateAccountTest extends BaseTest{
 	
 	@Test	//End to End Successful Registration on the Website.
-	public void testCreateAccountPage() {
+	public void TC01() {
 		CreateAccountPage cap = new CreateAccountPage(driver);
-		cap.CreateAccPage("John", "Sina", "John.Sina@dummy.com", "Ert@#$123");
+		cap.CreateAccPage("John", "Cina", "John.Cina@dummy.com", "Ert@#$123");
+	}
+	
+	@Test	//Validation for First Name
+	public void TC02() {
+		CreateAccountPage cap = new CreateAccountPage(driver);
+		cap.CreateAccPage("j0hn", "Cina", "John.Cina@dummy.com", "Ert@#$123");
+	}
+	
+	@Test	//Validation for Last Name
+	public void TC03() {
+		CreateAccountPage cap = new CreateAccountPage(driver);
+		cap.CreateAccPage("John", "c!n@", "John.Cina@dummy.com", "Ert@#$123");
+	}
+	
+	@Test	//Validation for Email Address
+	public void TC04() {
+		CreateAccountPage cap = new CreateAccountPage(driver);
+		cap.CreateAccPage("John", "Cina", "John.Cina", "Ert@#$123");
+	}
+	
+	@Test	//Validation for Password
+	public void TC05() {
+		CreateAccountPage cap = new CreateAccountPage(driver);
+		cap.CreateAccPage("John", "Cina", "John.Cina@dummy.com", "Ert123");
 	}
 
 }
